fdb21fc9df6e43d4e249c72d8d9d226788e07f25
assassin_box <assassin543@outlook.jp>
2021-03-14 16:39:50 +0900

    axiosインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App: React.FC = () => {
+  axios.get('http://localhost:4000/api/shop_accounts/');
   return <h1>tiffin</h1>;
 };
 
