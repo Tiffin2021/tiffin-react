d020c45742f07039f313b0788cb88101b340d7b7
hajime takeuchi <shadowthe8156@gmail.com>
2021-03-14 19:06:20 +0900

    Router導入&Aboutページまで作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 import Axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { SampleView } from 'src/views/Sample/SampleView';
+import { About } from 'src/views/About/About';
 
 Axios.defaults.baseURL = 'http://localhost:4000/api/';
 
 const App: React.FC = () => {
   return (
     <>
-      <SampleView />
+      <Router>
+        <Route exact path="/" component={SampleView} />
+        <Route path="/about" component={About} />
+      </Router>
     </>
   );
 };
---
+++ src/views/About/About.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const About: React.FC = () => {
+  return <h1>About Page!!!!</h1>;
+};
