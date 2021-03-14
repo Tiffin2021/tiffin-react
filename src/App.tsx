5c7ca5621df6baf7bec7c070f65865ef95dae4c8
hajime takeuchi <shadowthe8156@gmail.com>
2021-03-14 16:12:16 +0900

    linter導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>tiffin</h1>
-  );
-}
+const App: React.FC = () => {
+  return <h1>tiffin</h1>;
+};
 
 export default App;
