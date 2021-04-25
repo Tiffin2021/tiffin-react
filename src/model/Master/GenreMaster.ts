66aeb9e888813191f5f162adbcaa3853b02f2e44
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-04-25 21:49:34 +0900

    ジャンルをプルダウンに変更し、金額の最小値を0に設定

--- src/model/Master/GenreMaster.ts
+++ src/model/Master/GenreMaster.ts
@@ -2,3 +2,8 @@ export type GenreMaster = {
   id: number;
   genre: string;
 };
+
+export const initGenreMaster = {
+  id: 0,
+  genre: '選択されていません',
+};
--- src/views/PhotoRegister/PhotoRegister.tsx
+++ src/views/PhotoRegister/PhotoRegister.tsx
@@ -5,19 +5,21 @@ import { Photo, initPhoto } from 'src/model/Photo';
 import { ShopInfo, initShopInfo } from 'src/model/ShopInfo';
 import backgroundImage from 'src/pictures/businessBackground.jpg';
 import { Header } from 'src/components/Header/Header';
+import { initGenreMaster, GenreMaster } from 'src/model/Master/GenreMaster';
 
 export const PhotoRegister: React.FC = () => {
   //後からログイン情報のshopAccountIDを取得
   const shopAccountId = 1;
   const [photo, setPhoto] = useState(initPhoto);
   const [image, setImage] = useState<File>();
+  const [genreMasters, setGenreMasters] = useState([initGenreMaster]);
 
   useEffect(() => {
     (async () => {
-      // const response = await Axios.get<ShopInfo>(`shop_info/${shopAccountId}`);
-      // setShopInfo(response.data);
+      const genres = await Axios.get<GenreMaster[]>('genres');
+      setGenreMasters(genres.data);
     })();
-  }, []);
+  }, [setGenreMasters]);
 
   const changeName = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newPhoto = Object.assign({}, photo);
@@ -25,7 +27,7 @@ export const PhotoRegister: React.FC = () => {
     setPhoto(newPhoto);
   };
 
-  const changeGenre = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const changeGenre = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const newPhoto = Object.assign({}, photo);
     newPhoto.genre = e.target.value;
     setPhoto(newPhoto);
@@ -78,11 +80,22 @@ export const PhotoRegister: React.FC = () => {
             </div>
             <div className={style.formItem}>
               <div>ジャンル</div>
-              <input type="text" className={style.formInput} onChange={changeGenre} />
+              <select name="ジャンル" className={style.formInput} onChange={changeGenre}>
+                <option key="選択してください" value="選択してください">
+                  選択してください
+                </option>
+                {genreMasters.map((genreMaster) => {
+                  return (
+                    <option key={genreMaster.id} value={genreMaster.genre}>
+                      {genreMaster.genre}
+                    </option>
+                  );
+                })}
+              </select>
             </div>
             <div className={style.formItem}>
               <div>金額</div>
-              <input type="number" className={style.formInput} onChange={changePrice} />
+              <input type="number" className={style.formInput} onChange={changePrice} min="0" />
             </div>
             <div className={style.formItem}>
               <div>画像</div>
