cc4ca66938ffdd2371c5c55a091332e0384a3fcf
assassin_box <assassin543@outlook.jp>
2021-04-28 23:51:42 +0900

    画像ファイルを取り込みBASE64形式に変換

--- src/views/PhotoUpload/PhotoUpload.tsx
+++ src/views/PhotoUpload/PhotoUpload.tsx
@@ -5,13 +5,43 @@ import { Header } from 'src/components/Header/Header';
 
 export const PhotoUpload: React.FC = () => {
   const [photoDate, setPhotoDate] = useState('');
-  const changeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPhotoDate(e.target.value);
+
+  const getImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    const img: File = e.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(img);
+    reader.onload = function () {
+      console.info(reader.result);
+      if ('string' === typeof reader.result) {
+        setPhotoDate(reader.result);
+      }
+    };
+    reader.onerror = function (error) {
+      console.info('Error: ', error);
+    };
   };
 
-  const photoUpload = async () => {
+  const submitImage = () => {
     console.info(photoDate);
-    //history.push(`/`);
+    // const header = {
+    //   headers: {
+    //     'Content-Type': 'application/json;charset=UTF-8',
+    //     'Access-Control-Allow-Origin': '*',
+    //   },
+    // };
+    // const data = new FormData();
+    // data.append('file', photoDate);
+    // const imgUri = '任意のURL';
+    // axios
+    //   .post(imgUri, data, header)
+    //   .then((res) => {
+    //     //任意の処理
+    //   })
+    //   .catch((err) => {
+    //     //任意の処理
+    //   });
   };
 
   return (
@@ -22,10 +52,16 @@ export const PhotoUpload: React.FC = () => {
         <div className={style.backgroundForm}>
           <h2 className={style.pageTitle}>画像アップロード</h2>
           <div className={style.formItem}>
-            <input type="file" className={style.formInput} value={photoDate} onChange={changeEmail} />
+            <input
+              type="file"
+              className={style.formInput}
+              accept="image/*,.png,.jpg,.jpeg,.gif"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => getImage(e)}
+            />
           </div>
+          <img src={photoDate}></img>
           <div>
-            <button className={style.btn} onClick={photoUpload}>
+            <button className={style.btn} onClick={submitImage}>
               Update
             </button>
           </div>
